386a9e3bc1863daba051751270a12c6bc5653b53
CHANDRAKANT GAWANDE <102407610+cgawande@users.noreply.github.com>
2023-11-25 13:53:20 +0530

    Update webpack.config.js
    change

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       {
         test: /\.js|\.jsx$/,
         exclude: /node_modules/,
-        include: path.resolve(__dirname,),
+        include: path.resolve(__dirname,"src"),
         use: "babel-loader",
       },
       {
